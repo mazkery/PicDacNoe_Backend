5308562558be5eaa1b00c49a768cdd707c37bc47
Trương Quang Duy <truongquangduy343@gmail.com>
2020-12-09 18:51:17 +0700

    Create Connect Between client and server

--- app.js
+++ app.js
@@ -1,63 +1,64 @@
-const createError = require('http-errors');
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const passport = require('./src/services/user/login/passport');
-const cookieSession = require('cookie-session');
-require('dotenv').config();
-
-const indexRouter = require('./src/routes/index');
-const usersRouter = require('./src/routes/users');
+const createError = require("http-errors");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const passport = require("./src/services/user/login/passport");
+const cookieSession = require("cookie-session");
+require("dotenv").config();
+const cors = require("cors");
+const indexRouter = require("./src/routes/index");
+const usersRouter = require("./src/routes/users");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 // Session
 app.use(
-	cookieSession({
-		name: 'session',
-		keys: ['key1', 'key2'],
-	})
+  cookieSession({
+    name: "session",
+    keys: ["key1", "key2"],
+  })
 );
 
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // connect to MongoDB database
 const uri = process.env.MONGO_URI;
 mongoose.connect(uri, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 const connection = mongoose.connection;
-connection.once('open', () => {
-	console.log('MongoDB database connection established successfully.');
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully.");
 });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.send('Error');
+  // render the error page
+  res.status(err.status || 500);
+  res.send("Error");
 });
 
 module.exports = app;
