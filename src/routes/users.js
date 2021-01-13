5c6288577d3724589cca12fa4fb2c8240ca809a7
Trương Quang Duy <truongquangduy343@gmail.com>
2021-01-14 06:04:32 +0700

    socket chat

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
+const User = require("../model/users.model");
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-	res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
 module.exports = router;
--- src/services/socket/socket.js
+++ src/services/socket/socket.js
@@ -6,13 +6,10 @@ const saltRounds = 4;
 let onlineUsers = {};
 let onlineRooms = [];
 let headRoom = {};
+let subRoom = {};
 let opponent = {};
 let roomIn = {};
-let serverData = [
-  {
-    id: "id",
-  },
-];
+let roomMessages = {};
 let roomIdMapping = {};
 
 // Socket set up
@@ -26,9 +23,9 @@ const io = require("socket.io")(server, {
 // Socket Implement
 io.on("connection", (socket) => {
   //Them user vao danh sach online
-  socket.on("onlineUser", (email) => {
-    if (email != null) {
-      onlineUsers[socket.id] = email;
+  socket.on("onlineUser", (name) => {
+    if (name != null) {
+      onlineUsers[socket.id] = name;
     }
 
     io.emit("onlineList", onlineUsers);
@@ -38,73 +35,114 @@ io.on("connection", (socket) => {
   socket.on("createRoom", (displayRoomId) => {
     if (!onlineRooms.includes(displayRoomId)) {
       headRoom[socket.id] = displayRoomId;
+      roomIn[socket.id] = displayRoomId;
       onlineRooms.push(displayRoomId);
 
       bcrypt.hash(Date.now(), saltRounds, function (err, hash) {
         roomIdMapping[displayRoomId] = hash;
-        socket.join(hash);
+        socket.join(displayRoomId);
       });
+
+      //Tao danh sach message;
+      roomMessages[displayRoomId] = [];
 
       io.emit("onlineRooms", onlineRooms);
     }
   });
 
-  socket.on("joinRoom", (displayRoomId, email) => {
+  socket.on("joinRoom", (joinInfo) => {
+    let displayRoomId = joinInfo.roomId;
+    let name = joinInfo.name;
     // Kiem tra roomId ton tai va join vao room
     if (onlineRooms.includes(displayRoomId)) {
-      socket.join(roomIdMapping[displayRoomId]);
+      socket.join(displayRoomId);
+      roomIn[socket.id] = joinInfo.roomId;
     }
 
     //Neu room chua co doi thu thi assign doi thu
     if (!opponent.hasOwnProperty(displayRoomId)) {
       opponent[displayRoomId] = socket.id;
+      subRoom[socket.id] = displayRoomId;
 
       // emit ve room co the bat dau
-      io.emit(displayRoomId, email);
+      io.to(displayRoomId).emit(displayRoomId, {
+        label: "opponent",
+        name: name,
+      });
     }
   });
 
   socket.on("leaveRoom", (displayRoomId) => {
-    leaveRoomByUser(socket, displayRoomId);
+    leaveRoomByUser(io, socket, displayRoomId);
   });
 
   // Unregister cho tung thanh vien trong room (khong phai player)
   socket.on("removedRoom", (displayRoomId) => {
-    socket.leave(roomIdMapping.displayRoomId);
+    socket.leave(roomIdMapping[displayRoomId]);
+  });
+
+  socket.on("onServerLocalRoom", (message) => {
+    let displayRoomId = roomIn[socket.id];
+
+    handleRoomMessage(io, message, displayRoomId);
   });
 
   socket.on("disconnect", () => {
     let displayRoomId = roomIn[socket.id];
 
-    leaveRoomByUser(socket, displayRoomId);
+    leaveRoomByUser(io, socket, displayRoomId);
 
     delete onlineUsers[socket.id];
     io.emit("onlineList", onlineUsers);
   });
-
-  socket.on("send-message", (messagePackage) => {
-    io.to(messagePackage.room).emit("newMessage", messagePackage.text);
-  });
 });
+
+function handleRoomMessage(io, message, displayRoomId) {
+  // Tin nhan moi
+  if (message.label === "sendMessage") {
+    //Them tin nhan moi vao list
+    roomMessages[displayRoomId].push({
+      token: message.token,
+      name: message.name,
+      text: message.text,
+    });
+    // Cap nhat xuong toan bo client
+    io.to(displayRoomId).emit("localRoom", {
+      label: "updateMessage",
+      token: message.token,
+      name: message.name,
+      text: message.text,
+    });
+  }
+
+  // Lay toan bo tin nhan
+  if (message.label === "requestAllMessages") {
+    let allMessagesInfo = roomMessages[displayRoomId];
+
+    io.to(displayRoomId).emit("localRoom", {
+      label: "getAllMessages",
+      allMessages: allMessagesInfo,
+    });
+  }
+}
 
 function removeAllMember(io, displayRoomId) {
   //Emit ve fe de out room
-  io.emit("removedRoom", displayRoomId);
+  io.to(displayRoomId).emit("removedRoom", displayRoomId);
   //Xoa tham chieu
-  delete roomIdMapping.displayRoomId;
+  delete roomIdMapping[displayRoomId];
   //Xoa khoi onlineRooms
   let index = onlineRooms.indexOf(displayRoomId);
   onlineRooms.slice(index, 1);
 
   //Cap nhat lai danh sach onlineRooms cho client
   io.emit("onlineRooms", onlineRooms);
-  console.log(onlineRooms);
 }
 
-function leaveRoomByUser(socket, displayRoomId) {
+function leaveRoomByUser(io, socket, displayRoomId) {
   // Neu chu room roi phong thi xoa cac socket con lai trong room
   if (headRoom.hasOwnProperty(socket.id)) {
-    removeAllMember();
+    removeAllMember(io, displayRoomId);
   } else {
     //Neu la doi thu
     if (opponent[displayRoomId] === socket.id) {
