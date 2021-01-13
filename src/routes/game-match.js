7fc6ea7b0106c95b16355f9a34c7d475bb902de1
mazkery <bsicno999@gmail.com>
2021-01-13 16:41:11 +0700

    Update game match route

--- src/routes/game-match.js
+++ src/routes/game-match.js
@@ -1,26 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { createNewMatch, saveGameResult, getGameHistory } = require('../services/GameMatch');
+const { saveGameMatchAndResult, getGameHistory } = require('../services/GameMatch');
 
 /**
  * POST /game-match/save
  * {
- * 	"player1": user.id,
- *  "player2": user.id
- * }
- * */
-router.post('/create', createNewMatch);
-
-/**
- * POST /game-match/save
- * {
+ *  "player1": user.id,
+ *  "player2": user.id,
  * 	"gameId": "",
  *  "squares": "",
  *  "winline": "",
  * 	"winner": user.id,
  * }
  * */
-router.post('/save', saveGameResult);
+router.post('/save', saveGameMatchAndResult);
 
 /* GET /gameId */
 router.get('/:gameId', getGameHistory);
--- src/services/GameMatch/index.js
+++ src/services/GameMatch/index.js
@@ -2,61 +2,49 @@ const User = require('../../model/users.model');
 const GameMatch = require('../../model/gameMatch.model');
 
 /**
+ *
  * {
- * 	"player1": user.id,
- *  "player2": user.id
- * }
- * */
-exports.createNewMatch = async function (req, res, next) {
-	const player1Id = req.body.player1;
-	const player2Id = req.body.player2;
-	const player1 = await User.findById(player1Id);
-	if (!player1) return res.status(500).json({ message: 'Invaid Player 1 id.' });
-	const player2 = await User.findById(player2Id);
-	if (!player2) return res.status(500).json({ message: 'Invalid Player 2 id' });
-	const newGameMatch = await GameMatch.create(player1Id, player2Id);
-	player1.game.total += 1;
-	player1.game.matches.push(this);
-	await player1.save();
-	player2.game.total += 1;
-	player2.game.matches.push(this);
-	await player2.save();
-	return res.json({ message: 'New match created.', gamematch: newGameMatch });
-};
-
-/**
- * {
+ *  "player1": user.id,
+ *  "player2": user.id,
  * 	"gameId": "",
  *  "squares": "",
  *  "winline": "",
  * 	"winner": user.id,
  * }
- * */
-exports.saveGameResult = async function (req, res, next) {
-	const gameMatch = await GameMatch.findOne({ gameId: req.body.gameId }).exec();
-	if (!gameMatch) return res.status(500).json({ message: 'Match id not exist.' });
-	const player1 = await User.findById(req.body.player1);
-	const player2 = await User.findById(req.body.player2);
-	const winnerId = req.body.winner;
-	if (gameMatch.player1 === winnerId) {
-		gameMatch.winner = winnerId;
+ */
+exports.saveGameMatchAndResult = async function (req, res, next) {
+	const player1Id = req.body.player1;
+	const player2Id = req.body.player2;
+	const gameId = req.body.gameId;
+	const squares = req.body.squares;
+	const winline = req.body.winline;
+	const player1 = await User.findById(player1Id);
+	if (!player1) return res.status(500).json({ message: 'Invaid Player 1 id.' });
+	const player2 = await User.findById(player2Id);
+	if (!player2) return res.status(500).json({ message: 'Invalid Player 2 id' });
+	const newGameMatch = await GameMatch.create({ gameId, player1, player2, squares, winline });
+	if (newGameMatch.player1 === winnerId) {
+		newGameMatch.winner = winnerId;
+		newGameMatch.loser = newGameMatch.player2;
 		player1.game.win += 1;
 		player2.game.lose += 1;
-	} else if (gameMatch.player2 === winnerId) {
-		gameMatch.winner = winnerId;
+	} else if (newGameMatch.player2 === winnerId) {
+		newGameMatch.winner = winnerId;
+		newGameMatch.loser = newGameMatch.player1;
 		player2.game.win += 1;
 		player1.game.lose += 1;
-	}
-	// a draw
-	else {
+	} else {
+		// a draw
 		player2.game.draw += 1;
 		player1.game.draw += 1;
 	}
-	gameMatch.squares = req.body.squares;
-	gameMatch.winline = req.body.winline;
-	gameMatch.isActive = false;
-	const updatedGameMatch = await gameMatch.save();
-	return res.json({ message: 'Saved Game Result', gameMatch: updatedGameMatch });
+	player1.game.total += 1;
+	player1.game.matches.push(this);
+	player2.game.total += 1;
+	player2.game.matches.push(this);
+	await player1.save();
+	await player2.save();
+	return res.json({ message: 'Match history save.', gamematch: newGameMatch });
 };
 
 /**
@@ -64,5 +52,6 @@ exports.saveGameResult = async function (req, res, next) {
  * */
 exports.getGameHistory = async function (req, res, next) {
 	const gameMatch = await GameMatch.findOne({ gameId: req.params.gameId }).exec();
+	if (!GameMatch) return res.status(500).json({ message: 'Game match not found.' });
 	return res.json({ message: 'Fetch Game History', gameMatch });
 };
