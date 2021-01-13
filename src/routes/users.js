09b3d65a40e060c54cd6b955e13a73649a3ef618
mazkery <bsicno999@gmail.com>
2021-01-14 02:24:35 +0700

    user profile

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../model/users.model');
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-	res.send('respond with a resource');
+/* GET users listing. 
+	{
+		"email": ...
+	}
+*/
+router.get('/profile', async function (req, res, next) {
+	const email = req.body.email;
+	const user = User.findOne({ email });
+	if (!user) return res.status(500).json({ message: 'Cannot found user.' });
+	return res.json({ message: 'Found user successfully.', user });
 });
 
 module.exports = router;
