bde0ab7eb05eed1b490517c7a3e71ebfe23b50c2
mazkery <bsicno999@gmail.com>
2021-01-14 08:56:06 +0700

    fix syntax

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,6 @@
-const express = require("express");
-const User = require("../model/users.model");
+const express = require('express');
+const User = require('../model/users.model');
 const router = express.Router();
-const User = require("../model/users.model");
 
 /* users profile
 	POST /users/profile
@@ -9,14 +8,14 @@ const User = require("../model/users.model");
 		"email": ...
 	}
 */
-router.post("/profile", function (req, res, next) {
-  const email = req.body.email;
-  User.findOne({ email })
-    .then((user) => {
-      if (!user) return res.status(500).json({ message: "Cannot found user." });
-      return res.json({ message: "Found user successfully.", user });
-    })
-    .catch((error) => res.status(500).json({ message: error.message }));
+router.post('/profile', function (req, res, next) {
+	const email = req.body.email;
+	User.findOne({ email })
+		.then((user) => {
+			if (!user) return res.status(500).json({ message: 'Cannot found user.' });
+			return res.json({ message: 'Found user successfully.', user });
+		})
+		.catch((error) => res.status(500).json({ message: error.message }));
 });
 
 /**
@@ -25,31 +24,31 @@ router.post("/profile", function (req, res, next) {
  * 	"id"
  * }
  */
-router.post("/change-status", function (req, res, next) {
-  const id = req.body.id;
-  User.findById(id)
-    .then(async (user) => {
-      if (!user) return res.status(500).json({ message: "Cannot found user." });
-      user.active = !user.active;
-      const savedUser = await user.save();
-      return res.json({
-        message: "User status changed successfully.",
-        user: savedUser,
-      });
-    })
-    .catch((error) => res.status(500).json({ message: error.message }));
+router.post('/change-status', function (req, res, next) {
+	const id = req.body.id;
+	User.findById(id)
+		.then(async (user) => {
+			if (!user) return res.status(500).json({ message: 'Cannot found user.' });
+			user.active = !user.active;
+			const savedUser = await user.save();
+			return res.json({
+				message: 'User status changed successfully.',
+				user: savedUser,
+			});
+		})
+		.catch((error) => res.status(500).json({ message: error.message }));
 });
 
 /**
  * GET /users/all
  */
-router.get("/all", function (req, res, next) {
-  User.find({}, (err, users) => {
-    if (err) res.status(500).json({ message: err.message });
-    else {
-      res.json({ message: "Fetch all users.", count: users.length, users });
-    }
-  });
+router.get('/all', function (req, res, next) {
+	User.find({}, (err, users) => {
+		if (err) res.status(500).json({ message: err.message });
+		else {
+			res.json({ message: 'Fetch all users.', count: users.length, users });
+		}
+	});
 });
 
 module.exports = router;
